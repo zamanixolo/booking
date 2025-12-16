474390be4ed0f691bcd0d054e5713170619320d5
zamanixolo <zamani.xolo@gmail.com>
2025-12-16 12:15:43 +0200

    build file open next

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1765659254701,
+    __BUILD_TIMESTAMP_MS__: 1765880080693,
     __NEXT_BASE_PATH__: "/app",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
